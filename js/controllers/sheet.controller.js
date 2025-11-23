50b8db149b740adb6f695741592337045e6bfc6b
Issac Corona Córdoba <icc944476@gmail.com>
2025-11-22 22:13:08 -0600

    [IC]: Nombre de personajes autoañadible

--- js/controllers/sheet.controller.js
+++ js/controllers/sheet.controller.js
@@ -10,10 +10,16 @@ app.controller("sheet_controller", function($scope){
         'shot':'scene'
     };
 
+    $scope.character_names = [];
+
+    //@ === Variables Helpers ====
     const types = Object.keys(map_types);
     const $editor_badge = document.querySelector('#badge');
     $editor_badge.textContent = "scene";
-    
+    $box_suggestion = document.getElementById('suggestions');
+    $box_sugg_pos = -1; 
+
+
     //+ ==== Helpers ====
     function nextType(current_type){
         return map_types[current_type];
@@ -31,6 +37,14 @@ app.controller("sheet_controller", function($scope){
 
     function updateState(state){
         $editor_badge.textContent = state;
+    }
+
+    function newCharacter(character){
+        if ($scope.character_names.includes(character)){
+            return
+        }
+        $scope.character_names.push(character);
+        console.log($scope.character_names);
     }
 
     //+ === Logica === 
@@ -85,7 +99,22 @@ app.controller("sheet_controller", function($scope){
 
             switch(true){
                 case line.textContent.trim() === '':
-                    next_type = 'scene';
+                    next_type = 'action';
+                    break;
+
+                case line.dataset.type === 'character':
+                    //+ Revisar si selecciono un character ya definido
+                    let exist_suggestion = $box_suggestion.querySelector('.highlight');
+
+                    if ((line.textContent.length > 2) && (exist_suggestion === null)){
+                        newCharacter(line.textContent);
+                    }else if(exist_suggestion){
+                        line.textContent = exist_suggestion.textContent;
+                    }
+
+                    $box_suggestion.style.display = "none";
+                    next_type = 'dialogue';
+                    updateState(next_type);
                     break;
 
                 case line.dataset.type === 'dialogue':
@@ -94,6 +123,11 @@ app.controller("sheet_controller", function($scope){
                 
                 case line.dataset.type === 'parenthetical':
                     next_type = 'dialogue';
+                    updateState(next_type);
+                    break;
+
+                case (line.dataset.type === 'action') && !(line.textContent.trim() === ''):
+                    next_type = 'action';
                     updateState(next_type);
                     break;
 
@@ -111,7 +145,7 @@ app.controller("sheet_controller", function($scope){
             return;
         }
         
-        if (e.key === 'Tab'){
+        if(e.key === 'Tab'){
             e.preventDefault();
 
             const line = getCurrentLine(); if (!line) return;
@@ -132,13 +166,33 @@ app.controller("sheet_controller", function($scope){
             line.dataset.type = next_type;
             updateState(next_type);
         }
+
+        if(['ArrowUp','ArrowDown'].includes(e.key)){
+            //* Validamos que este posicionado sobre character y que exista una recomendacion abierta
+            //* Si es el caso, manejamos el evento
+            if(($editor_badge.textContent == 'character') && ($box_suggestion.style.display === "block")){
+                e.preventDefault();
+                switch(true){
+                    case e.key === 'ArrowUp' && $box_sugg_pos >= 1:
+                        $box_sugg_pos-=1;
+                        break;
+                    case e.key === 'ArrowDown' && $box_sugg_pos < ($box_suggestion.childNodes.length - 1):
+                        $box_sugg_pos+=1;
+                        break;
+                }
+                
+                const items = $box_suggestion.querySelectorAll(".item-suggestion");
+                items.forEach((node, idx)=>{
+                    idx === $box_sugg_pos ? node.classList.add("highlight"):node.classList.remove("highlight");
+                });
+            }
+        }
     });
 
     SHEET.addEventListener('keyup',(e)=>{
         if(['ArrowUp','ArrowDown'].includes(e.key)){
             e.preventDefault();
             const line = getCurrentLine(); if (!line) return;
-            
             updateState(line.dataset.type);
         }
 
@@ -146,6 +200,57 @@ app.controller("sheet_controller", function($scope){
             const line = getCurrentLine(); if (!line) return;
             updateState(line.dataset.type);
         }
+
+        if($editor_badge.textContent == 'character'){
+            //+ Usamos condicional para solo filtrar el la seccion de character
+
+            const line = getCurrentLine();
+            const text = line.textContent.toLowerCase();
+            
+            if(text.length < 1){ $box_suggestion.style.display="none"; return }
+            
+            //* Buscar coincidencias
+            const matches = $scope.character_names.filter(x => x.startsWith(text));
+            if(matches.length <= 0){$box_suggestion.style.display="none"; return} // No mostrar si no hay
+            
+            //* Dejar de mostrar si hay coincidencia 100% o no existe
+            if(matches.length === 1){
+                if (matches[0].length === text.length){
+                    $box_suggestion.style.display="none"; 
+                    return
+                }
+            }
+
+            if($box_suggestion.style.display==='block' && ['ArrowUp','ArrowDown'].includes(e.key)){
+                //* Evitar que se sobrescriba la caja 
+                return
+            }
+
+            //* Posicionarlo justo debajo
+            const rect = line.getBoundingClientRect();
+            $box_suggestion.style.left = rect.left + "px";
+            $box_suggestion.style.top = rect.bottom + "px";
+            $box_suggestion.style.display = "block";
+
+            //* Añadir sugerencias
+            $box_suggestion.innerHTML = "";
+            matches.forEach(name => {
+                const item = document.createElement('div');
+                item.classList.add('item-suggestion');
+                item.textContent = name;
+                item.style.padding = "4px";
+
+                item.addEventListener("mousedown", ()=>{
+                    line.innerHTML = name;
+                    $box_suggestion.style.display = "none";
+                });
+
+                $box_suggestion.appendChild(item);
+            });
+
+            $box_sugg_pos=-1;
+        }
+
     });
 
 
