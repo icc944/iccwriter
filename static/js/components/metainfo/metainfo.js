f8077d75f41575e8c608de8de801b7b5e09d0308
Issac Corona Cordoba <icc944476@gmail.com>
2026-03-21 18:52:37 -0600

    [IC]: Termino un boton mejor

--- static/js/components/metainfo/metainfo.js
+++ static/js/components/metainfo/metainfo.js
@@ -14,6 +14,7 @@ angular.module('Writter')
             scope.project_description = scope.projectDescription;
             scope.project_meta = scope.projectMeta;
 
+            scope.isMetaOpen = true;
         }
     }
 }]);
